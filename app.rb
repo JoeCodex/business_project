5a7de1bb237edc5fa1724ccadbf3f50b082ce34a
Sidney Wagner <herrwagner1680@gmail.com>
2014-10-30 16:39:20 -0400

    adding app.rb mandrill link

--- app.rb
+++ app.rb
@@ -1,24 +1,26 @@
 require "sinatra"
 require "mandrill"
 
-# get "/" do
-# 	m = Mandrill::API.new
-# 	message = {
-# 		:subject=> "Hello from the Mandrill API",
-# 		:from_name=> "Your name",
-# 		:text=>"Hi message, how are you?",
-# 		:to=>[
-# 			{
-# 				:email=> "swagner2001@mac.com",
-# 				:name=> "HerrWagner"
-# 				}
-# 			],
-# 			:html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",
-# 			:from_email=>"sender@yourdomain.com"
-# 	}
-# 		sending = m.messages.send message
-# 		puts sending
-# end
+get "/sent" do
+	m = Mandrill::API.new
+	message = {
+		:subject=> "Hello from the Mandrill API",
+		:from_name=> "Your name",
+		:text=>"Hi message, how are you?",
+		:to=>[
+			{
+				:email=> "swagner2001@mac.com",
+				:name=> "HerrWagner"
+				}
+			],
+			:html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",
+			:from_email=>"sender@yourdomain.com"
+	}
+		sending = m.messages.send message
+		puts sending
+
+		erb :sent
+end
 
 get '/' do 
 
